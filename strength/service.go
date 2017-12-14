98561f91168ab36ad8f16c0307a6236c9ab5c64e
Michael Shamasa <michael@parsable.com>
2017-12-14 14:58:24 -0800

    added sqlite database

--- strength/service.go
+++ strength/service.go
@@ -1,6 +1,10 @@
 package strength
 
-import "time"
+import (
+  "database/sql"
+  "fmt"
+  _ "github.com/mattn/go-sqlite3"
+)
 // import "errors"
 
 // StrengthService handles returning info
@@ -16,22 +20,57 @@ type StrengthList []Strength
 type WorkoutList []Workout
 
 func (strengthService) Index() StrengthList {
-  workoutList := WorkoutList {
-    Workout { Name: "Squat" },
-    Workout { Name: "Deadlift" },
-    Workout { Name: "Bench Press" },
-    Workout { Name: "Overhead Press" },
-    Workout { Name: "Barbell Row" },
+  workoutList := WorkoutList {}
+
+  db, err := sql.Open("sqlite3", "./strength.db")
+  checkErr(err)
+  if err != nil {
+    fmt.Println("could not connect to database.")
   }
+  defer db.Close()
+  // query
+  rows, err := db.Query("SELECT * FROM workouts")
+  checkErr(err)
+  var userId string
+  var exercise int32
+  var weight float32
+  var sets int32
+  var reps int32
+  var completed int32
+  var date int32
+
+  for rows.Next() {
+    err = rows.Scan(&userId, &exercise, &weight, &sets, &reps, &completed, &date)
+    checkErr(err)
+
+    workout := Workout {
+      UserId: userId,
+      Exercise: exercise,
+      Weight: weight,
+      Sets: sets,
+      Reps: reps,
+      Completed: completed,
+      Date: date,
+    }
+    workoutList = append(workoutList, workout)
+  }
+
+  rows.Close()
+  db.Close()
 
   strengthList:= StrengthList {
     Strength {
-      Date: time.Now().Unix(),
       WorkoutList: workoutList,
     },
   }
 
   return strengthList;
+}
+
+func checkErr(err error) {
+  if err != nil {
+    panic(err)
+  }
 }
 
 // var Error = errors.New("Shit fucked yo!")
--- strength/structs.go
+++ strength/structs.go
@@ -6,8 +6,11 @@ type Strength struct {
 }
 
 type Workout struct {
-  Name      string    `json:"name"`
-  Completed bool      `json:"completed"`
-  Sets      int       `json:"sets"`
-  Reps      int       `json:"reps"`
+  UserId    string    `json:userId`
+  Exercise  int32     `json:"exercise"`
+  Weight    float32   `json:"weight"`
+  Sets      int32     `json:"sets"`
+  Reps      int32     `json:"reps"`
+  Completed int32     `json:"completed"`
+  Date      int32     `json:"date"`
 }

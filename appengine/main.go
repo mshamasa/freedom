cee6ca937d287c2ef1495cac1ff9218b7c63b988
Michael Shamasa <michael@parsable.com>
2018-01-06 15:17:00 -0800

    added CORS stuff

--- appengine/main.go
+++ appengine/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"net/http"
+	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/mshamasa/freedom/strength"
 	"google.golang.org/appengine"
@@ -12,12 +14,16 @@ func main() {
 	router := mux.NewRouter()
 
 	router.Methods("GET").PathPrefix("/strength/{userID}").Handler(strength.IndexHandler)
-	router.Methods("PUT").PathPrefix("/strength/addRows").Handler(strength.AddRowsHandler)
+	router.Methods("POST").PathPrefix("/strength/addRows").Handler(strength.AddRowsHandler)
 	router.Methods("PUT").PathPrefix("/strength/saveWorkout").Handler(strength.SaveWorkoutHandler)
 	router.Methods("PUT").PathPrefix("/strength/updateDate").Handler(strength.UpdateDateHandler)
 	router.Methods("DELETE").PathPrefix("/strength/deleteRow").Handler(strength.DeleteRowHandler)
 
-	http.Handle("/", router)
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type"})
+	originsOk := handlers.AllowedOrigins([]string{os.Getenv("ORIGIN_ALLOWED")})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"})
+
+	http.Handle("/", handlers.CORS(headersOk, originsOk, methodsOk)(router))
 	appengine.Main()
 
 	// log.Fatal(http.ListenAndServe(":8080", router))
